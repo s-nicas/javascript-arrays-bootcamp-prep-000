3e218e441bbcf5ee379bfbd602cf1b22692088ff
Sonia  D. Nicas <sduarte22@gmail.com>
2018-03-02 04:31:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element){
-  return array.unshift(element);
+  return array.unshift('element');
 }
 
 function addElementToEndOfArray(array, element){
@@ -13,5 +13,5 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
+  array.push('element');
 }
