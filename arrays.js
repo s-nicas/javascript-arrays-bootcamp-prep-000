f4de5117b8b555e203e4f6790c26c2fde138653b
Sonia  D. Nicas <sduarte22@gmail.com>
2018-03-02 04:42:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,5 +19,18 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 
 function accessElementInArray(array, index){
-  return array[index]
+  return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+
+
+
+
+
+
+
