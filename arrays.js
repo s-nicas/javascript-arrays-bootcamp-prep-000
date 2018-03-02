5a99713a551fb2753cfb55e32ba1ff8e00e79116
Sonia  D. Nicas <sduarte22@gmail.com>
2018-03-02 04:18:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,4 +4,6 @@ function addElementToBeginningOfArray(array, element){
   return ['element',...array ];
 }
 
-function destructiveA
+function destructivelyAddElementToBeginningOfArray (array, element){
+  return array.push(element)
+}
