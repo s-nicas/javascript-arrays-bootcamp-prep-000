3993dc5ca6629523e7b5c324c7330da464f38d81
Sonia  D. Nicas <sduarte22@gmail.com>
2018-03-02 04:46:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,10 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 
-
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array ;
+}
 
 
 
